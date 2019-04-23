07b6a39fc8c21d0d41844c07f643c0fa8402283c
snowood1 <yibohu2013@gmail.com>
2019-04-23 13:27:02 -0500

    Update and rename SPN_419.py to SPN_scratch.py

---
+++ SPN_scratch.py
@@ -0,0 +1,241 @@
+import numpy as np
+
+class Node(object):
+    def __init__(self, id, children=[],parents=[]):
+        self.id = id
+        self.parents = parents
+        self.children = children
+        self.scope= set()
+        self.update_links()
+        self.value=0
+        self.derivative = 0
+
+    def update_links(self):
+        for child in self.children:
+            self.scope = self.scope.union(child.scope)
+            if self not in child.parents:
+                child.add_parent(self)
+
+    def add_child(self,node):
+        pass
+
+    def add_parent(self,parent):
+        self.parents.append(parent)
+
+    def eval(self):
+        pass
+
+    def pass_gradient(self):
+        pass
+
+
+class SumNode(Node):
+    def __init__(self, id, children=[],weights=[],parents=[]):
+
+        assert all(isinstance(x, ProdNode) for x in children), 'Wrong child nodes'
+        assert all(x.scope == children[0].scope for x in children), 'Children with an identical scope'
+        assert len(weights)==len(children) or weights==[], 'Wrong dimension of weights'
+
+        Node.__init__(self, id, children,parents)
+        self.weights = weights if weights else np.ones(len(self.children))
+
+    def add_child(self,node,weight=1):
+        assert isinstance(node, ProdNode), 'Wrong child nodes'
+        self.children.append(node)
+        node.add_parent(self)
+        self.weights.append(weight)
+
+        #     for child
+        # Todo:  MPE Inference (MPE assignment)  MPE 没有写，只有sum 模式
+
+    def eval(self, X, Q, mpe=None):  #  X = {x1:0, x2:0 , x3:1}
+        weights = self.weights/np.sum(self.weights)
+
+        if mpe == None:
+            s= np.dot(weights, [child.eval(X,Q) for child in self.children])
+        self.value = s
+        return self.value
+
+    def pass_gradient(self):    # sum node 对child  pass gradient
+        weights = self.weights/np.sum(self.weights)
+        self.weights = weights + weights * self.derivate
+        for child, w in self.children, self.weights:
+            child.derivative = w
+
+class ProdNode(Node):
+    def __init__(self, id, children=[],parents=[]):
+
+        assert all(isinstance(x, (SumNode,LeafNode)) for x in children), 'Wrong child nodes'
+        assert self.all_disjoint(children), 'Children with disjoint scopes'
+
+        Node.__init__(self, id, children,parents)
+
+    def all_disjoint(self,children):
+        sets = [x.scope for x in children]
+        union = set().union(*sets)
+        n = sum(len(u) for u in sets)
+        return n == len(union)
+
+    def add_child(self,node):
+        assert isinstance(node,(SumNode,LeafNode)), 'Wrong child nodes'
+        node.add_parent(self)
+
+    def eval(self, X, Q):  #  X = {x1:0, x2:0 , x3:1}
+        p = 1.0
+        for child in self.children:
+            p *= child.eval(X,Q)
+        self.value = p
+        return self.value
+
+    def pass_gradient(self):    
+        for child in self.children:
+            child.derivative = child.derivative + self.derivative * \
+                    np.prod([sib.value for sib in self.children if sib != child])
+
+class LeafNode(Node):
+    def __init__(self, id, variable,weights=[], parents=[]):
+
+        '''
+        One leaf node represent one variable
+        :param id: variable names, eg X1
+        :param values: The values of the variable, eg X1=0, X1= 1 binary
+        :param weights: The weights/probabilities of the values P(X1=0)= 0.2, P(X1=1) = 0.8
+        '''
+        assert isinstance(variable, Variables), 'Input a variable class'
+        assert len(weights)==len(variable.domain) or weights==[], 'Wrong dimension of weights'
+
+        self.id = id
+        self.variable =variable
+        self.domain = variable.domain
+        self.weights = weights if weights else np.ones(len(self.domain))
+        self.scope = {variable.id}
+        self.parents = parents
+        self.value= 0
+        self.derivate = 0
+
+    def eval(self, X, Q):
+        '''
+        :param X:  means N X K data (np.array) with features x0 x1 x2 ...xk-1
+        :param Q:  a vector represents which features are queried.
+                    For example [0 1 0] means we only want to know P(x1)
+                    [1,1,0] means P(x0,x1)
+        :return:  np.array of values
+        '''
+
+        scope = list(self.scope)[0] #  The scope means this leaf nodes contain x?
+
+        if Q[scope]==1:
+            weights = self.weights/np.sum(self.weights)
+            values = {k: w for k,w in zip(self.domain,weights)}
+            self.value = np.vectorize(values.get)(X[:,scope])
+            # print('self.value',self.value)
+        else:
+            self.value = np.ones(len(X))
+            # print(' X self.value',self.value)
+
+        return self.value
+
+    def get_gradient(self):
+        return
+    def pass_gradient(self):
+        weights = self.weights/np.sum(self.weights)
+        self.weights = weights + weights * self.derivate
+
+
+class Variables():
+    def __init__(self,id,name='x',domain=[0,1]):
+        self.id=id
+        self.name = name+str(id)
+        self.domain = domain
+    def set_domain(self,domain):
+        self.domain= domain
+        
+        
+class SPN():
+    def __init__(self, nodes=[]):
+        self.nodes = nodes
+        self.leaves = {}
+        self.root = None
+
+    #Todo:
+    def train(self):
+
+        Var=[]
+
+        for k in range(2):
+            Var.append(Variables(k))   # 定义了两个binary variables  x0, x1
+
+        s1 = LeafNode('s1',Var[0],weights=[2,8])  # 叶子 s1 包含 x0
+        s2 = LeafNode('s2',Var[0] ,weights=[1,9])  # s2 包含 x0
+        s3 = LeafNode('s3',Var[1],weights=[4,6]) # s3 包含 x1
+        s4 = LeafNode('s4',Var[1],weights=[3,7]) # s4 包含 x1
+
+        p1 = ProdNode('p1 = s1+s3', [s1, s3])  # 第二层是product node  
+        p2 = ProdNode('p2 = s2+s4', [s2, s4])
+
+        s0 = SumNode('root', [p1,p2],[0.3,0.7]) # 第三层是root sum node
+
+        self.nodes=[s1,s2,s3,s4, p1,p2,s0]
+        self.root= s0
+
+    def inference(self,X,query,evidence=None): 
+        '''
+        # P(Q|E)= ?  Can output marginal and joint probability
+
+        :param X: training data
+        :param query:  [1,1,0] means P(x0,x1)   
+        :param evidence:  [1,0,0] means P(x0)
+        :return:  marginal probability  or conditional probability
+        '''
+        if evidence:
+            query = np.array(query) + np.array(evidence)        # P(x2| x1 ) = P(x2,x1 )/P(x1 )
+            return self.root.eval(X,query)/self.root.eval(X,evidence)
+        else:
+            return self.root.eval(X,query)
+
+    def gradient_descent(self):
+        self.root.gradient = 1
+
+        print('\nBefore gradient descent:')
+        for node in self.nodes:
+            print(node.id, node.gradient)
+
+        self.root.pass_gradient()
+        print('\nAfter gradient descent:')
+        for node in self.nodes:
+            print(node.id, node.gradient)
+
+
+
+# 实现了简单的inference 过程
+s=SPN()
+s.train()
+
+# X是training data  一共四行，每行两个feature x0 x1
+#            x0 x1
+X=np.array([[0,0],     #P(x0=0, x1 = 0 )
+            [0,1],     #P(x0=0, x1 = 1 )
+            [1,0],     #P(x0=1, x1 = 0 )
+            [1,1]])    #P(x0=1, x1 = 1 )
+
+print('\nP(x0,x1):')
+print(s.inference(X,[1,1]))  # [1,1] means P(x0, x1) 意思是考虑x0,x1,联合概率
+
+print('\nP(x0):')
+print(s.inference(X,[1,0]))  # [1,0] means P(x0) 意思是考虑x0, 不考虑 x1, marginal prob
+
+print('\nP(x1):')
+print(s.inference(X,[0,1]))  # [0,1] means P(x1) 意思是考虑x1, 不考虑 x0, marginal prob
+
+# P(x1| x0 )  第一个[0,1]表示查询P(x1), 第二个[1,0] 表示条件是P(x0)
+print('\nP(x1| x0 ):')
+print(s.inference(X,[0,1],[1,0]))
+
+# P(x0| x1 )  第一个[1,0] 表示查询P(x0), 第二个[0,1] 表示条件是P(x1)
+print('\nP(x0|x1):')
+print(s.inference(X,[1,0],[0,1]))
+
+# 现在我们看一下gradient decent的结果， 做gradient必须先inference了之后才能做gradient
+print('\nP(x0,x1) gradient decent:')
+s.inference(X,[1,1])
+s.gradient_descent()
