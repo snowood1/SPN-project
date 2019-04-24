500fe18c43915d27a3475d3885ea63d9ba4a1990
leodd <410528518@qq.com>
2019-04-24 15:52:19 -0500

    Change file name

--- SPNDemo.py
+++ SPNDemo.py
@@ -1,13 +1,11 @@
 from SumProductNets import *
-from NetGenerator import NetGenerator, goThrough, save2file
-from Preprocessing import get_data_loader, generate_data
 import numpy as np
-import os
+
 
 # each column is a data point
 data = np.array([
-    [1, 0, 0],
-    [0, 1, 0]
+    [1, 0, 0, 1, 1],
+    [0, 1, 0, 1, 1]
 ])
 
 # create random variables
@@ -21,17 +19,14 @@ X3 = RVNode(rv1)
 X4 = RVNode(rv2)
 
 rv_list = [rv1, rv2]
-test_gen = NetGenerator(2, rv_list, sum_replicate=(2, 6), prod_replicate=(2, 6))
 
 # build SPN tree
 S_prod1 = ProductNode([X1, X2])
 S_prod2 = ProductNode([X3, X4])
 
-# root = SumNode([S_prod1, S_prod2])
-root = test_gen.generate()
+root = SumNode([S_prod1, S_prod2])
 
 S = SPN(root, [rv1, rv2])
-# print(goThrough(S.root))
 
 # compute the join probability
 print(S.prob([rv1, rv2], data))
@@ -47,21 +42,3 @@ S.train(data, iterations=1000, step_size=5)
 S.print_weight()
 
 print(S.prob([rv1, rv2], data))
-
-# Demo code for MNIST data set
-
-rv_list = [RV((0, 1), 'x' + str(i)) for i in range(788)]
-test_gen = NetGenerator(788, rv_list, sum_replicate=2, prod_replicate=4)
-
-root = test_gen.generate()
-S = SPN(root, rv_list)
-
-mnist_dl = get_data_loader()
-
-for f, l in mnist_dl:
-    inter = generate_data(f, l)
-    S.train(inter, iterations=100, step_size=5)
-
-# S.print_weight()
-save2file(os.getcwd() + "/spn_with_weight.obj", S)
-
