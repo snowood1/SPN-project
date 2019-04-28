660d326a8159216b55dfe328a74f6167bc601f3a
alanlin <linyu0077@gmail.com>
2019-04-28 13:05:45 -0500

    1. CLear the code & add cut_limit tuple

--- NetGenerator.py
+++ NetGenerator.py
@@ -11,7 +11,7 @@ import numpy as np
 
 # Network generator -- generate network with single parent
 class NetGenerator(object):
-    def __init__(self, num_feature, number_label, rv_list, sum_replicate=2, prod_replicate=2):
+    def __init__(self, num_feature, number_label, rv_list, sum_replicate=2, prod_replicate=2, cut_limit=None):
         '''
         Store the information that generator used to generate network
         :param num_feature: number of random variables of input data
@@ -31,13 +31,13 @@ class NetGenerator(object):
         self.rv_list = rv_list
         self.sum_rep = sum_replicate
         self.prod_rep = prod_replicate
-        self.start_point_list = None
+        self.cut_limit = cut_limit
 
     def generate(self, mode='forest'):
         if mode == 'forest':
             return self._create_node(self.features, True)
         elif mode == 'generic':
-            assert isinstance(self.prod_rep, tuple)
+            assert self.cut_limit is not None
             return self._generic_structure(4)
 
     def _create_node(self, var_list, sum_node=True):
@@ -91,15 +91,14 @@ class NetGenerator(object):
                 return SumNode(children)
             else:
                 sub_blocks = self._decompose(var_block, block_type='coarse',
-                                             max_cut=np.random.randint(self.prod_rep[0], self.prod_rep[1] + 1))
+                                             max_cut=np.random.randint(self.cut_limit[0], self.cut_limit[1] + 1))
                 for sb in sub_blocks:
                     children.append(self._create_node_v2(sb, sum_node=True, coarse_mode=True))
                 return ProductNode(children)
         else:
             # Handle the feature matrix in fine grain mode
             if e_r - s_r == 1 and e_c - s_c == 1:
-                var_name = self.features_matrix[s_r, s_c]
-                return RVNode(self.rv_list[var_name])
+                return RVNode(self.rv_list[self.features_matrix[s_r, s_c]])
 
             children = list()
             if sum_node:
@@ -108,55 +107,18 @@ class NetGenerator(object):
                 return SumNode(children)
             else:
                 sub_blocks = self._decompose(var_block, block_type='fine',
-                                             max_cut=np.random.randint(self.prod_rep[0], self.prod_rep[1] + 1))
+                                             max_cut=np.random.randint(self.cut_limit[0], self.cut_limit[1] + 1))
 
                 for sb in sub_blocks:
                     children.append(self._create_node_v2(sb, sum_node=True, coarse_mode=False))
                 return ProductNode(children)
 
-    def _generic_structure(self, coarse_solution, fine_solution=1):
-        # Generate the general network structure of SPN -- Complex version
-        # Namely, every node can only have one parent
-        # K = sum_replicate
-
+    def _generic_structure(self, coarse_solution):
         assert self.img_size % coarse_solution == 0
-        # coarse_img_size = self.img_size // coarse_solution
-        #
-        # coarse_solution_map = dict()
-        # fine_solution_map = dict()
-        #
-        # # Generate all possible coarse resolution img decomposition
-        # for _w in range(1, coarse_img_size + 1):
-        #     for _h in range(1, coarse_img_size + 1):
-        #         # Generate trivial decomposition
-        #         if _w == 1 and _h == 1:
-        #             continue
-        #
-        #         # Generate non-trivial decomposition
-        #         for _w_s in range(0, self.img_size - _w * coarse_solution + 1, coarse_solution):
-        #             for _h_s in range(0, self.img_size - _h * coarse_solution + 1, coarse_solution):
-        #                 # Name current block by top_left and bottom_right pixel (y_s, x_s, y_e, x_e)
-        #                 tmp_key = (_h_s, _w_s, _h_s + _h * coarse_solution, _w_s + _w * coarse_solution)
-        #                 coarse_solution_map[tmp_key] = self._coordinate2vr(tmp_key)
-        #
-        # if self.start_point_list is None:
-        #     self._generate_start_points(coarse_solution)
-        #
-        # # Generate trivial decomposition -- That is, (coarse_resolution * coarse_resolution) block
-        # for _w in range(1, coarse_solution + 1):
-        #     for _h in range(1, coarse_solution + 1):
-        #
-        #         for _w_s in range(0, coarse_solution - _w + 1):
-        #             for _h_s in range(0, coarse_solution - _h + 1):
-        #                 for (sy, sx) in self.start_point_list:
-        #                     tmp_key = (sy + _h_s, sx + _w_s, sy + _h_s + _h, sx + _w_s + _w)
-        #                     fine_solution_map[tmp_key] = self._coordinate2vr(tmp_key)
-
         return self._create_node_v2((0, 0, self.img_size, self.img_size), sum_node=True, coarse_mode=True)
 
     def _coordinate2vr(self, coordinate):
-        res = self.features_matrix[coordinate[0]:coordinate[2], coordinate[1]:coordinate[3]].flatten().tolist()
-        return res
+        return self.features_matrix[coordinate[0]:coordinate[2], coordinate[1]:coordinate[3]].flatten().tolist()
 
     def _generate_start_points(self, coarse_solution):
         res = []
@@ -170,75 +132,59 @@ class NetGenerator(object):
     def _decompose(self, in_block, block_type='coarse', coarse_solution=4, max_cut=4):
         # block_type would be coarse & fine
         sub_blocks = [in_block]
+
         if block_type == 'fine':
             for _ in range(max_cut):
-                target_block_pivot = np.random.randint(0, len(sub_blocks))
-                target_block = sub_blocks[target_block_pivot]
-                if (target_block[2] - target_block[0] <= 1) and (target_block[3] - target_block[1] <= 1):
+                target_block_idx = np.random.randint(0, len(sub_blocks))
+                (s_r, s_c, e_r, e_c) = sub_blocks[target_block_idx]
+
+                if (e_r - s_r == 1) and (e_c - s_c == 1):
+                    # Reach elemental pixel, give up this cut
                     continue
 
                 cut_type = np.random.random()
-                if (cut_type > .5 and target_block[3] - target_block[1] > 1) or \
-                        (cut_type <= .5 and target_block[2] - target_block[0] <= 1):
-                    # Cut vertically & there exists a legal cut line
-                    cut_line = np.random.randint(target_block[1] + 1, target_block[3])
-
-                    p1, p2 = (target_block[0], target_block[1], target_block[2], cut_line), \
-                             (target_block[0], cut_line, target_block[2], target_block[3])
-
-                    sub_blocks[target_block_pivot] = p1
-                    sub_blocks.append(p2)
+                if (cut_type > .5 and e_c - s_c > 1) or (cut_type <= .5 and e_r - s_r == 1):
+                    # Cut vertically, exist a legal cut in column or no legal cut line in row
+                    cut_line = np.random.randint(s_c + 1, e_c)
+                    p1, p2 = (s_r, s_c, e_r, cut_line), (s_r, cut_line, e_r, e_c)
                 else:
-                    # Cut horizontally
-                    if target_block[2] - target_block[0] <= 1:
+                    # Cut horizontally otherwise
+                    if e_r - s_r == 1:
                         continue
 
-                    cut_line = np.random.randint(target_block[0] + 1, target_block[2])
-                    p1, p2 = (target_block[0], target_block[1], cut_line, target_block[3]), \
-                             (cut_line, target_block[1], target_block[2], target_block[3])
+                    cut_line = np.random.randint(s_r + 1, e_r)
+                    p1, p2 = (s_r, s_c, cut_line, e_c), (cut_line, s_c, e_r, e_c)
 
-                    sub_blocks[target_block_pivot] = p1
-                    sub_blocks.append(p2)
+                sub_blocks[target_block_idx] = p1
+                sub_blocks.append(p2)
         elif block_type == 'coarse':
             for _ in range(max_cut):
-                target_block_pivot = np.random.randint(0, len(sub_blocks))
-                target_block = sub_blocks[target_block_pivot]
+                target_block_idx = np.random.randint(0, len(sub_blocks))
+                (s_r, s_c, e_r, e_c) = sub_blocks[target_block_idx]
 
-                if (target_block[2] - target_block[0] <= coarse_solution) and \
-                        (target_block[3] - target_block[1] <= coarse_solution):
+                if (e_r - s_r <= coarse_solution) and (e_c - s_c <= coarse_solution):
+                    # Reach elemental pixel, give up this cut
                     continue
 
                 cut_type = np.random.random()
-                if (cut_type > .5 and target_block[3] - target_block[1] > coarse_solution) or \
-                        (cut_type <= .5 and target_block[2] - target_block[0] <= coarse_solution):
+                if (cut_type > .5 and e_c - s_c > coarse_solution) or (cut_type <= .5 and e_r - s_r <= coarse_solution):
                     # Cut vertically & there exists a legal cut line
-
-                    cut_seed = np.random.randint(1, (target_block[3] - target_block[1]) // coarse_solution)
-                    cut_line = target_block[1] + coarse_solution * cut_seed
-
-                    p1, p2 = (target_block[0], target_block[1], target_block[2], cut_line), \
-                             (target_block[0], cut_line, target_block[2], target_block[3])
-
-                    sub_blocks[target_block_pivot] = p1
-                    sub_blocks.append(p2)
+                    cut_line = s_c + coarse_solution * np.random.randint(1, (e_c - s_c) // coarse_solution)
+                    p1, p2 = (s_r, s_c, e_r, cut_line), (s_r, cut_line, e_r, e_c)
                 else:
                     # Cut horizontally
-                    if target_block[2] - target_block[0] <= coarse_solution:
+                    if e_r - s_r <= coarse_solution:
                         continue
 
-                    cut_seed = np.random.randint(1, (target_block[2] - target_block[0]) // coarse_solution)
-                    cut_line = target_block[0] + coarse_solution * cut_seed
+                    cut_line = s_r + coarse_solution * np.random.randint(1, (e_r - s_r) // coarse_solution)
+                    p1, p2 = (s_r, s_c, cut_line, e_c), (cut_line, s_c, e_r, e_c)
 
-                    p1, p2 = (target_block[0], target_block[1], cut_line, target_block[3]), \
-                             (cut_line, target_block[1], target_block[2], target_block[3])
-
-                    sub_blocks[target_block_pivot] = p1
-                    sub_blocks.append(p2)
+                sub_blocks[target_block_idx] = p1
+                sub_blocks.append(p2)
         else:
             raise Exception('Unknown Block_Type presented!!!')
 
         return sub_blocks
-
 
 
 def save2file(path, obj):
@@ -267,18 +213,15 @@ def goThrough(node):
 
 def main():
     test_rv = [RV(domain=[0, 1]) for _ in range(28*28)]
-    test_gen = NetGenerator(28*28, 0, test_rv, sum_replicate=2, prod_replicate=(5, 10))
-    # test_gen._generic_structure(4)
-    # root = test_gen.generate()
-    # test_spn = SPN(root, test_rv)
-    # goThrough(test_spn.root)
-    # save2file(os.getcwd() + "/test.json", test_spn)
-    # print(goThrough(test_spn.root))
-    # test_spn = read_from_file(os.getcwd() + "/test.json", SPN)
-    # print(goThrough(test_spn.root))
+    test_rv_2 = [RV(domain=[0, 1]) for _ in range(28*28)]
+    # Generic construction requires sum_replicate & cut_limit
+    test_gen = NetGenerator(28*28, 0, test_rv, sum_replicate=2, cut_limit=(5, 10))
+    # Random construction requires sum_replicate & prod_replicate
+    test_gen_2 = NetGenerator(28*28, 0, test_rv_2, prod_replicate=5, cut_limit=(1, 1))
+
     root = test_gen.generate(mode='generic')
     print(goThrough(root))
-    root2 = test_gen.generate(mode='forest')
+    root2 = test_gen_2.generate(mode='forest')
     print(goThrough(root2))
 
 
