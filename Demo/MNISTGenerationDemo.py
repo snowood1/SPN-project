417f4ed1426a44f9503072834a8c446773cfc153
leodd <410528518@qq.com>
2019-05-10 14:27:16 -0500

    add data set

--- Demo/MNISTGenerationDemo.py
+++ Demo/MNISTGenerationDemo.py
@@ -1,1 +1,9 @@
-from mlxtend
+from mlxtend.data import loadlocal_mnist
+
+
+X, Y = loadlocal_mnist(
+    images_path='./Data/train-images.idx3-ubyte',
+    labels_path='./Data/train-labels.idx1-ubyte'
+)
+
+print(X.shape)
