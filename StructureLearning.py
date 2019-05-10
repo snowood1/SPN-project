1cfde184b25d4e086f3805f8a1101f821c9d7c98
leodd <410528518@qq.com>
2019-05-09 21:58:47 -0500

    structure learning completed

--- StructureLearning.py
+++ StructureLearning.py
@@ -1,9 +1,12 @@
 from SumProductNets import *
 import numpy as np
+from sklearn.metrics import mutual_info_score
 
 
-k_mean_k = 3
+k_mean_k = 2
 k_mean_its = 20
+
+dependent_threshold = 0.05
 
 
 def train(data, rvs):
@@ -36,7 +39,35 @@ def split_instances(data, rvs):
 
 
 def split_variables(data, rvs):
-    pass
+    d, m = data.shape
 
+    ch = list()
+
+    remaining_rvs_idx = set(range(d))
+    for x_idx in range(d):
+        if x_idx in remaining_rvs_idx:
+            ch_rvs_idx = [x_idx]
+            remaining_rvs_idx -= {x_idx}
+            for y_idx in range(d):
+                if y_idx in remaining_rvs_idx:
+                    mi = mutual_info(data[x_idx], data[y_idx], rvs[x_idx], rvs[y_idx])
+                    if mi > dependent_threshold:
+                        remaining_rvs_idx -= {y_idx}
+                        ch_rvs_idx.append(y_idx)
+
+            if len(ch_rvs_idx) == 1 or (ch_data == ch_data[0]).all():
+                for i in ch_rvs_idx:
+                    ch.append(RVNode(rvs[i]))
+            else:
+                ch_data = data[ch_rvs_idx, :]
+                ch.append(split_instances(ch_data, [rvs[i] for i in ch_rvs_idx]))
+
+    return ProductNode(ch)
+
+
+def mutual_info(x, y, rv_x, rv_y):
+    c_xy, _, _ = np.histogram2d(x, y, bins=(rv_x, rv_y))
+    res = mutual_info_score(None, None, contingency=c_xy)
+    return res
 
 
